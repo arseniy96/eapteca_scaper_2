6c8b82be7f18b0156a6c4d5545cb39cbc7494e53
arsenii <arsenzhar@gmail.com>
2020-07-15 19:32:15 +0300

    Config

--- config/application.rb
+++ config/application.rb
@@ -3,14 +3,14 @@ require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
-require "sprockets/railtie"
+# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -29,5 +29,7 @@ module EaptekaScraper2
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.autoload_paths += %W(#{config.root}/app)
   end
 end
